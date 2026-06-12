47b559c6eb1432a74a6342265425767c0bbeddf0
Rigel Ramadhani Waloni <rigel8911@gmail.com>
2026-06-12 09:52:32 +0700

    Initial Commit

---
+++ LyricBar/LyricBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricBarApp.swift
+//  LyricBar
+//
+//  Created by Rigel Ramadhani Waloni on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct LyricBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
